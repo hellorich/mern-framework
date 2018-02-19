52b187005a13d8da1b28fce3a8c5af529ef5a743
Rich Cookson <rich.cookson@gmail.com>
2018-02-19 15:56:39 +0000

    Set up initial configuration of webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const clientConfig = {
+  context: path.resolve(__dirname, 'client'),
+
+  devtool: 'eval-source-map',
+
+  entry: './app.jsx',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'app.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015'],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = [clientConfig];
